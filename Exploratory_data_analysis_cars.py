810c2c9fe7ffe7306abed76b0122e72998ebb06f
SafwatJahin <144806007+SafwatJahin@users.noreply.github.com>
2024-04-06 23:02:11 +0200

    Update Exploratory_data_analysis_cars.py

--- Exploratory_data_analysis_cars.py
+++ Exploratory_data_analysis_cars.py
@@ -1,1 +1,15 @@
+## Opjectives
 
+# Explore features or characteristics to predict price of car
+# Analyze patterns and run descriptive statistical analysis
+# Group data based on identified parameters and create pivot tables
+# Identify the effect of independent attributes on price of cars
+
+# Import libraries:
+import pandas as pd
+import numpy as np
+
+# Load the data and store it in dataframe df:
+path='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv'
+df = pd.read_csv(path)
+df.head()
